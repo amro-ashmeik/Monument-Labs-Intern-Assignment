1fe2341a2e3cac7b756429eabb1f5de0a30e487d
amro-ashmeik <amroashmeik2020@u.northwestern.edu>
2018-05-29 13:09:45 -0500

    Added error handling.

--- task2.py
+++ task2.py
@@ -8,12 +8,12 @@ try:
 	                              host='127.0.0.1',
 	                              database='users')
 except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Incorrect username or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Incorrect username or password")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database does not exist")
+	else:
+		print(err)
 
 cursor = cnx.cursor()
 query = ("SELECT * FROM user")
@@ -25,3 +25,9 @@ for (id, name, email) in cursor:
 
 cursor.close()
 cnx.close()
+
+try:
+	print(gi)
+except mysql.error as err:
+	print(what)
+
